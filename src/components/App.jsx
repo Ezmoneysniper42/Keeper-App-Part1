c6cedd623f381ebfbb4f69163c38f24bddc109fc
Ezmoneysniper42 <93779199+Ezmoneysniper42@users.noreply.github.com>
2022-07-21 12:44:29 +0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <html>
+      <head></head>
+
+      <body>
+        <Heading />
+        <Note />
+        <Footer />
+      </body>
+    </html>
+  );
+}
+
+export default App;
